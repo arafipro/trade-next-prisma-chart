d8ad727603a9067aac90cb8b852f0f9cfdfa1d7b
Hiroki Chiyoda <contact@arafipro.com>
2023-09-16 07:27:43 +0900

    Vercelにデプロイする準備#71

--- lib/stockApi.ts
+++ lib/stockApi.ts
@@ -3,6 +3,7 @@ const BaseUrl = "/api/stocks";
 export async function getAllStocks() {
   const res = await fetch(`${BaseUrl}`, {
     cache: "no-cache",
+    next: { revalidate: 10 },
   });
   const resData = await res.json();
   return resData.stocks;
--- lib/tradeApi.ts
+++ lib/tradeApi.ts
@@ -3,6 +3,7 @@ const BaseUrl = "/api/trades";
 export async function getAllTrades() {
   const res = await fetch(`${BaseUrl}`, {
     cache: "no-cache",
+    next: { revalidate: 10 },
   });
   const resData = await res.json();
   return resData.trades;
