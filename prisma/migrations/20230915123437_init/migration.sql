739d4d216e441cddc2a492def9f5c5939be21b7c
Hiroki Chiyoda <contact@arafipro.com>
2023-09-15 21:58:29 +0900

    データベースをSqliteからVercel Postgresに変更#61

---
+++ prisma/migrations/20230915123437_init/migration.sql
@@ -0,0 +1,25 @@
+-- CreateTable
+CREATE TABLE "Stock" (
+    "code" INTEGER NOT NULL,
+    "stockname" TEXT NOT NULL,
+    "market" TEXT NOT NULL,
+
+    CONSTRAINT "Stock_pkey" PRIMARY KEY ("code")
+);
+
+-- CreateTable
+CREATE TABLE "Trade" (
+    "id" SERIAL NOT NULL,
+    "price" DOUBLE PRECISION NOT NULL,
+    "shares" INTEGER NOT NULL,
+    "tradingDate" TEXT NOT NULL,
+    "stockCode" INTEGER NOT NULL,
+
+    CONSTRAINT "Trade_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Stock_stockname_key" ON "Stock"("stockname");
+
+-- AddForeignKey
+ALTER TABLE "Trade" ADD CONSTRAINT "Trade_stockCode_fkey" FOREIGN KEY ("stockCode") REFERENCES "Stock"("code") ON DELETE RESTRICT ON UPDATE CASCADE;
