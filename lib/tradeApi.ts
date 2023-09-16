466c5c701eb43196040c9d23e422d1c20c36c8e2
Hiroki Chiyoda <contact@arafipro.com>
2023-09-16 16:40:07 +0900

    tradeのデータをAPI経由から直接取得へ#77

--- lib/tradeApi.ts
+++ lib/tradeApi.ts
@@ -1,9 +1,13 @@
-const BaseUrl = "/api/trades";
+import prisma from "@/lib/prisma";
 
 export async function getAllTrades() {
-  const res = await fetch(`${BaseUrl}`, {
-    next: { revalidate: 10 },
+  const trades = await prisma.trade.findMany({
+    orderBy: {
+      tradingDate: "asc",
+    },
+    include: {
+      stock: true,
+    },
   });
-  const resData = await res.json();
-  return resData.trades;
+  return trades;
 }
