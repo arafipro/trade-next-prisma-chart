8349c3a6afbf9a503c5fe7a998a68495afb0e3e0
Hiroki Chiyoda <contact@arafipro.com>
2023-09-17 21:11:04 +0900

    コンポーネントPieChartPlotlyに引数を追加#83

--- components/PieChartPlotly.tsx
+++ components/PieChartPlotly.tsx
@@ -2,31 +2,54 @@
 "use client";
 
 import dynamic from "next/dynamic";
-import { Layout, PlotData } from "plotly.js";
+import { Layout } from "plotly.js";
 // ビルド時のエラーを回避するために追加
 const Plot = dynamic(() => import("react-plotly.js"), { ssr: false });
 
-const data: Partial<PlotData> = {
-  values: [100, 300, 600],
-  labels: ["Residential", "Non-Residential", "Utility"],
-  type: "pie",
-  textinfo: "label+percent",
-};
+// const data: Partial<PlotData> = {
+//   values: [100, 300, 600],
+//   labels: ["Residential", "Non-Residential", "Utility"],
+//   type: "pie",
+//   textinfo: "label+percent",
+// };
 
 const layout: Partial<Layout> = {
   height: 800,
   width: 800,
   title: {
-    text: "Test Pie Chart by Plotly",
-    font: { size: 30, family: "Noto Sans JP" },
+    text: "銘柄別投資額割合",
+    font: {
+      size: 30,
+      family: "Noto Sans JP",
+    },
   },
-  font: { family: "Noto Sans JP" },
+  font: {
+    size: 14,
+    family: "Noto Sans JP",
+  },
 };
 
-export default function PieChartPlotly() {
+export default function PieChartPlotly({
+  values,
+  labels,
+}: {
+  values: number[] | string[];
+  labels: number[] | string[];
+}) {
   return (
     <div>
-      <Plot data={[data]} layout={layout} />
+      <Plot
+        data={[
+          {
+            values: values,
+            labels: labels,
+            type: "pie",
+            textinfo: "label+percent",
+            textposition: "outside",
+          },
+        ]}
+        layout={layout}
+      />
     </div>
   );
 }
