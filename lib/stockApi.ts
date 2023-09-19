9f70527fcf2bc5fd419c0ae1b768dd17b1c0a50f
Hiroki Chiyoda <contact@arafipro.com>
2023-09-19 11:15:33 +0900

    コンポーネントPieChartPlotlyに渡す引数を取得するための関数を作成#91

--- lib/stockApi.ts
+++ lib/stockApi.ts
@@ -1,6 +1,18 @@
-import prisma from "@/lib/prisma";
+import { prisma } from "@/lib/prisma";
 
 export async function getAllStocks() {
   const stocks = await prisma.stock.findMany();
   return stocks;
+}
+
+export async function getStock(code: number) {
+  const stock = await prisma.stock.findMany({
+    select: {
+      stockname: true,
+    },
+    where: {
+      code: code,
+    },
+  });
+  return stock;
 }
--- lib/tradeApi.ts
+++ lib/tradeApi.ts
@@ -1,5 +1,6 @@
-import { prismaTrade } from "./prisma";
+import { prisma, prismaTrade } from "./prisma";
 
+// 全取引を取得
 export async function getAllTrades() {
   const trades = await prismaTrade.trade.findMany({
     orderBy: {
@@ -10,4 +11,52 @@ export async function getAllTrades() {
     },
   });
   return trades;
+}
+
+// 保有株式の証券コードを取得
+export async function getStockAllCodes() {
+  const allCodes = await prisma.trade.groupBy({
+    by: ["stockCode"],
+    orderBy: {
+      stockCode: "asc",
+    },
+  });
+  return allCodes;
+}
+
+// 銘柄別の全所有株数を取得
+export async function getStockAllShares() {
+  const allShares = await prisma.trade.groupBy({
+    by: ["stockCode"],
+    _sum: {
+      shares: true,
+    },
+    orderBy: {
+      stockCode: "asc",
+    },
+  });
+  return allShares;
+}
+
+// SELECT sum(shares * price) FROM trade WHERE stockCode = code
+// 上記のSQLを実現するPrisma Schemaがない模様
+// Prisma Schemaでshares * priceの計算まで実現
+// shares * priceの合計をforEachで取得
+export async function getTotalPriceByCode(code: number) {
+  // 指定した証券コードのすべての取引価格を取得
+  const trades = await prismaTrade.trade.findMany({
+    select: {
+      tradePrice: true,
+    },
+    where: {
+      stockCode: code,
+    },
+  });
+
+  // 取得した取引価格の合計を取得
+  let totalPrice = 0;
+  trades.forEach((trade) => {
+    totalPrice += trade.tradePrice;
+  });
+  return totalPrice;
 }
