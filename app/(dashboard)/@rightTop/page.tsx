6f0f4d4cd38a7a168d79d2ca826202fc881f5cb2
Hiroki Chiyoda <contact@arafipro.com>
2023-09-19 11:18:53 +0900

    コンポーネントPieChartPlotlyに引数を渡す#93

--- app/(dashboard)/@rightTop/page.tsx
+++ app/(dashboard)/@rightTop/page.tsx
@@ -1,9 +1,15 @@
 import PieChartPlotly from "@/components/PieChartPlotly";
+import { getValues } from "@/lib/tradeArgument";
 
-export default function Page() {
+export default async function Page() {
+  const res = await getValues();
+
+  const codes = res[0];
+  const stocknames = res[1];
+  const tradePrices = res[2];
   return (
-    <main className="flex items-center justify-center w-full">
-      <PieChartPlotly></PieChartPlotly>
+    <main className="flex items-center justify-center w-full ">
+      <PieChartPlotly values={tradePrices} labels={stocknames}></PieChartPlotly>
     </main>
   );
 }
