bfd6d15a404f933baf2f4fd1d4ee367e9f9ed442
Hiroki Chiyoda <contact@arafipro.com>
2023-10-15 23:30:59 +0900

    コンポーネントPieChartに引数を渡す#105

--- app/(dashboard)/@rightTop/page.tsx
+++ app/(dashboard)/@rightTop/page.tsx
@@ -1,9 +1,15 @@
 import PieChart from "@/components/PieChart";
+import { getValues } from "@/lib/tradeArgument";
 
-export default function Page() {
+export default async function Page() {
+	const res = await getValues();
+
+  const codes = res.codes;
+  const stocknames = res.stocknames;
+  const tradePrices = res.tradePrices;
   return (
     <main className="flex items-center justify-center w-full">
-      <PieChart></PieChart>
+      <PieChart values={tradePrices} labels={stocknames}></PieChart>
     </main>
   );
 }
