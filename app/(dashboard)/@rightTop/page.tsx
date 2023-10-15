24a0ceb6c05fc935a7c1b25569ae3f5fd8d1747c
Hiroki Chiyoda <contact@arafipro.com>
2023-10-15 23:18:45 +0900

    コンポーネントPieChartをレスポンシブに対応#101

--- app/(dashboard)/@rightTop/page.tsx
+++ app/(dashboard)/@rightTop/page.tsx
@@ -2,7 +2,7 @@ import PieChart from "@/components/PieChart";
 
 export default function Page() {
   return (
-    <main className="flex items-center justify-center">
+    <main className="flex items-center justify-center w-full">
       <PieChart></PieChart>
     </main>
   );
